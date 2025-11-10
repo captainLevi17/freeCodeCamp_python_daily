ee5d271f5e3142b0f3023811ac7f560014c0b508
CaptainLevi17 <migz.levi@gmail.com>
2025-11-10 22:39:16 +1100

    Word counter

---
+++ word_counter.py
@@ -0,0 +1,11 @@
+'''
+Word Counter
+Given a sentence string, return the number of words that are in the sentence.
+
+Words are any sequence of non-space characters and are separated by a single space.
+
+'''
+
+def count_words(sentence):
+    words = sentence.split(" ")
+    return len(words)
