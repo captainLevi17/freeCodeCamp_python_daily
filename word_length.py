7ca7e6d300404691f9dcf2d89373d35ad5720431
captainLevi17 <migz.levi@gmail.com>
2026-03-23 17:26:11 +1100

    Word Length Converter

---
+++ word_length.py
@@ -0,0 +1,13 @@
+'''
+Word Length Converter
+Given a string of words, return a new string where each word is replaced by its length.
+
+Words in the given string will be separated by a single space
+Keep the spaces in the returned string.
+For example, given "hello world", return "5 5".
+'''
+def convert_words(s):
+
+    words = s.split()
+    lengths = [str(len(word)) for word in words]
+    return " ".join(lengths)
